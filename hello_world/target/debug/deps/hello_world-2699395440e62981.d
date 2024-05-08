cd48c3bc86c9151ad6048a7fdc7b2405c7ff9e3c
Damilola Belllo <damilolabello4real@gmail.com>
2024-05-08 20:45:08 +0100

    hello world

---
+++ hello_world/target/debug/deps/hello_world-2699395440e62981.d
@@ -0,0 +1,5 @@
+/Users/damilolabelllo/Documents/Personal/rust-tryouts/hello_world/target/debug/deps/hello_world-2699395440e62981: src/main.rs
+
+/Users/damilolabelllo/Documents/Personal/rust-tryouts/hello_world/target/debug/deps/hello_world-2699395440e62981.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/damilolabelllo/Documents/Personal/rust-tryouts/hello_world/target/debug/hello_world: /Users/damilolabelllo/Documents/Personal/rust-tryouts/hello_world/src/main.rs
